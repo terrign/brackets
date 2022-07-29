2142675cfc2fd065a623353edd9229056705f630
terrign <vajipi@gmail.com>
2022-07-29 15:47:22 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let resultString = str
+  let configArr = bracketsConfig.map (a => a.join(''))
+  let configLength = configArr.length   
+  function removeBracketPair () {
+      for (let i = 0; i < configLength; i++) {
+          if (resultString.includes(configArr[i])){
+              resultString = resultString.replace(configArr[i], '')
+          }
+      }
+      let includeCounter = 0
+      for (let i = 0; i < configLength; i++) {
+          if (resultString.includes(configArr[i])) {
+              includeCounter++
+          }
+      }
+      if (includeCounter > 0) {
+          return removeBracketPair ()
+      } else return      
+  } 
+  removeBracketPair()
+  return resultString.length == 0 ? true : false 
 }
